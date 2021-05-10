4527be7a2494203979b474fe5c02b6685941a700
icpz <y@icpz.dev>
2021-05-10 22:29:22 +0800

    refine 7-zip

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -1,4 +1,4 @@
-class M7zip < Formula
+class Sevenzip < Formula
   desc "7-Zip is a file archiver with a high compression ratio"
   homepage "https://7-zip.org"
   url "https://7-zip.org/a/7z2102-src.7z"
@@ -6,12 +6,24 @@ class M7zip < Formula
   sha256 "ee9755feaa034e7075712bc6b1fad25440bd0a8f21f106a16e37bc4b409f1122"
   license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
 
+  on_linux do
+    depends_on "gcc@9" => :build
+  end
+
   def install
     cd "CPP/7zip/Bundles/Alone2" do
-      suffix = Hardware::CPU.arm? ? "arm64" : "x64"
-      system "make", "-f", "../../cmpl_mac_" + suffix + ".mak"
+      on_macos do
+        suffix = Hardware::CPU.arm? ? "arm64" : "x64"
+        system "make", "-f", "../../cmpl_mac_#{suffix}.mak"
 
-      bin.install "b/m_" + suffix + "/7zz"
+        bin.install "b/m_#{suffix}/7zz"
+      end
+
+      on_linux do
+        system "make", "CXX=#{Formula["gcc@9"].opt_bin}/g++-9", "-f", "../../cmpl_gcc.mak"
+
+        bin.install "b/g/7zz"
+      end
     end
   end
 
