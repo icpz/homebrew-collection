2d725808e409240f57b74a4b85fdefab439e1038
icpz <y@icpz.dev>
2022-05-11 23:13:18 +0800

    replace rz/sz path with homebrew-ed path

--- Formula/iterm2-zmodem.rb
+++ Formula/iterm2-zmodem.rb
@@ -4,9 +4,14 @@ class Iterm2Zmodem < Formula
   license "GPL-3.0"
   head "https://github.com/robberphex/iTerm2-zmodem.git"
 
+  depends_on "lrzsz"
+
   keg_only "should not be called directly"
 
   def install
+    inreplace "iterm2-recv-zmodem.sh", "/usr/local/bin/rz", "#{Formula["lrzsz"].opt_bin}/rz"
+    inreplace "iterm2-send-zmodem.sh", "/usr/local/bin/sz", "#{Formula["lrzsz"].opt_bin}/sz"
+
     bin.install "iterm2-recv-zmodem.sh"
     bin.install "iterm2-send-zmodem.sh"
   end
